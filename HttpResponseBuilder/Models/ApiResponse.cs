09b10dc9ee044943087d4eaa0aba44a7fae1d1ca
shubham0205sharma <shubham0205sharma@gmail.com>
2024-12-30 19:06:17 +0530

    Updated comments in model class.

--- HttpResponseBuilder/Models/ApiResponse.cs
+++ HttpResponseBuilder/Models/ApiResponse.cs
@@ -10,10 +10,10 @@ namespace HttpResponseBuilder.Models
     {
         public bool IsSuccess { get; set; } // Indicates whether the operation was successful
         public string Status { get; set; } // Optional, e.g., "success" or "error" etc.
-        public int StatusCode { get; set; } // HTTP status code for the response
-        public T Data { get; set; } // Main payload for successful responses
-        public ApiError Error { get; set; } // Error details for failed responses
-        public MetaData Metadata { get; set; } // Additional metadata
+        public int StatusCode { get; set; } // HTTP status code for the response.
+        public T Data { get; set; } // Main payload for successful responses.
+        public ApiError Error { get; set; } // Error details for failed responses.
+        public MetaData Metadata { get; set; } // Additional metadata.
 
         public ApiResponse()
         {
